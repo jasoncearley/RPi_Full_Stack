15287d006f6c0e16d2043d4b561044bd42bfeb45
jasoncearley <Jason@Jasons-MacBook.local>
2016-04-20 21:37:11 -0400

    Test of Git integration
    Testing use of Git Desktop, Atom, and Github.com

--- lab_app.py
+++ lab_app.py
@@ -1,3 +1,4 @@
+#Testing Git
 from flask import Flask, request, render_template
 import time
 import datetime
@@ -15,20 +16,20 @@ def lab_temp():
 	import Adafruit_DHT as DHT
 	import RPi.GPIO as GPIO
 	import sys
-	
+
 	Sensor = 11
 	humiture = 17
 
 	humidity, temperature = DHT.read_retry(Sensor, humiture)
-		
+
 	temperature = temperature * 9/5.0 + 32
 
 	if humidity is not None and temperature is not None:
 		return render_template("lab_temp.html",temp=temperature,hum=humidity)
 	else:
 		return render_template("no_sensor.html")
-		
-@app.route("/lab_env_db", methods=['GET']) 
+
+@app.route("/lab_env_db", methods=['GET'])
 def lab_env_db():
 	temperatures, humidities, from_date_str, to_date_str = get_records()
 	return render_template("lab_env_db.html",temp=temperatures,hum=humidities)
@@ -36,14 +37,14 @@ def lab_env_db():
 def get_records():
 	#Get the from date value from the URL
 	from_date_str = request.args.get('from',time.strftime("2016-01-01 00:00"))
-	
+
 	#Get the to date value from the URL
 	to_date_str = request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))
-	
+
 	# Validate date before sending it to the DB
 	if not validate_date(from_date_str):
 		from_date_str = time.strftime("%Y-%m-%d 00:00")
-	
+
 	# Validate date before sending it to the DB
 	if not validate_date(to_date_str):
 		to_date_str	= time.strftime("%Y-%m-%d %H:%M")
@@ -57,7 +58,7 @@ def get_records():
 	humidities 		= curs.fetchall()
 	conn.close()
 	return [temperatures, humidities, from_date_str, to_date_str]
-	
+
 def validate_date(d):
     try:
         datetime.datetime.strptime(d, '%Y-%m-%d %H:%M:%S')
