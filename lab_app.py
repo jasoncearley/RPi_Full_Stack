b0619e3f8df4ef850fc5dd0f3c5fa7120631f3d8
jasoncearley <Jason@Jasons-MacBook.local>
2016-04-23 20:36:51 -0400

    jquery addition and accounted for no sensor attached to RPi

--- lab_app.py
+++ lab_app.py
@@ -20,7 +20,14 @@ def lab_temp():
 	Sensor = 11
 	humiture = 17
 
-	humidity, temperature = DHT.read_retry(Sensor, humiture)
+	#humidity, temperature = DHT.read_retry(Sensor, humiture)
+
+	# If you don't have a sensor but still wish to run this program, comment out all the
+	# sensor related lines, and uncomment the following lines (these will produce random
+	# numbers for the temperature and humidity variables):
+	import random
+	humidity = random.randint(1,100)
+	temperature = random.randint(10,30)
 
 	temperature = temperature * 9/5.0 + 32
 
@@ -41,6 +48,17 @@ def get_records():
 	#Get the to date value from the URL
 	to_date_str = request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))
 
+	#This will return a string, if field range_h exists in the request
+	range_h_form	= request.args.get('range_h','');
+
+	#Initialise this variable with not a number
+	range_h_int 	= "nan"
+
+	try:
+		range_h_int	= int(range_h_form)
+	except:
+		print "range_h_form not a number"
+
 	# Validate date before sending it to the DB
 	if not validate_date(from_date_str):
 		from_date_str = time.strftime("%Y-%m-%d 00:00")
@@ -48,6 +66,14 @@ def get_records():
 	# Validate date before sending it to the DB
 	if not validate_date(to_date_str):
 		to_date_str	= time.strftime("%Y-%m-%d %H:%M")
+
+	# If range_h is not defined, we don't need the from and to times
+	if isinstance(range_h_int,int):
+		time_now		= datetime.datetime.now()
+		time_from 		= time_now - datetime.timedelta(hours = range_h_int)
+		#time_to   		= time_now
+		from_date_str   = time_from.strftime("%Y-%m-%d %H:%M")
+		to_date_str	    = time_now.strftime("%Y-%m-%d %H:%M")
 
 	import sqlite3
 	conn=sqlite3.connect('/var/www/lab_app/lab_app.db')
